e057910ab4b82240775313db8a5c50608faf5ff9
Alex Manzi <alex.manzi22@gmail.com>
2022-01-17 16:13:29 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+        }
+    return cards
+}
+writeCards(cards)
+
+
+function countDown() {
+    let j = 10;
+    while (j >= 0) {
+        console.log(j)
+        j--
+    }
+}
+countDown()
