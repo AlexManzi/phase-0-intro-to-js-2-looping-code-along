9b38eb4eee47296d4b99976fda55f2cddc7886e3
Alex Manzi <alex.manzi22@gmail.com>
2022-02-02 11:42:06 -0500

    commit tree 6d9bdc6f1304e0f0870d644521d47f4391b10b2e

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const cards = ["Guadalupe", "Ollie", "Aki"];
-function writeCards(cards) {
-    for (let i = 0; i < cards.length; i++) {
-        console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
-        }
-    return cards
+const cards = (["Guadalupe", "Ollie", "Aki"], "surprise");
+function writeCards(names, eventName) {
+    let newCards =[]
+    for (let i = 0; i < names.length ; i++) {
+        newCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newCards
 }
-writeCards(cards)
 
 
 function countDown() {
-    let j = 10;
-    while (j >= 0) {
-        console.log(j)
-        j--
+    let i = 11;
+    while (i > 0) {
+        i--
+        console.log(i)
+        }
     }
-}
 countDown()
+
